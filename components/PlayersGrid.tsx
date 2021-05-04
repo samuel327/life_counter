a26e26eb2f47160b67808330593dd8fb64ba1c1a
Samuel Holmes <samuelholmes30@gmail.com>
2021-05-03 23:35:44 -0500

    adding layout features

--- components/PlayersGrid.tsx
+++ components/PlayersGrid.tsx
@@ -4,18 +4,46 @@ export const PlayersGrid = () => {
   return (
     <View style={styles.content}>
       <View style={{ flex: 1, backgroundColor: 'blue', flexDirection: 'row' }}>
-        <View style={{ flex: 1, backgroundColor: 'orange' }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'green',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <Text>Player 1</Text>
         </View>
-        <View style={{ flex: 1, backgroundColor: 'purple' }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'red',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <Text>Player 2</Text>
         </View>
       </View>
       <View style={{ flex: 1, backgroundColor: 'green', flexDirection: 'row' }}>
-        <View style={{ flex: 1, backgroundColor: 'blue' }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'blue',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <Text>Player 3</Text>
         </View>
-        <View style={{ flex: 1, backgroundColor: 'black' }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'black',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <Text style={{ color: 'white' }}>Player 4</Text>
         </View>
       </View>
