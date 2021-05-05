3b107873419352097c992c9c279ffe64daefad47
Samuel Holmes <samuelholmes30@gmail.com>
2021-05-05 14:44:25 -0500

    modal layout

--- components/modals/LayoutModal.tsx
+++ components/modals/LayoutModal.tsx
@@ -1,5 +1,7 @@
+import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+
+import { CustomButton } from '../CustomButton';
 import React from 'react';
-import { Button, View, Text, StyleSheet, Modal } from 'react-native';
 
 interface LayoutModalProps {
   visible: boolean;
@@ -16,13 +18,39 @@ export const LayoutModal = (props: LayoutModalProps) => {
   return (
     <Modal visible={visible}>
       <View style={styles.content}>
-        <Text>Layout Modal</Text>
+        <Text
+          style={{ fontSize: 22, alignSelf: 'flex-start', marginLeft: '12%' }}
+        >
+          Choose a layout:{' '}
+        </Text>
         <View style={styles.btns}>
-          <Button title='Columnar' onPress={() => handleSelect('columnar')} />
-          <Button title='Grid' onPress={() => handleSelect('grid')} />
-        </View>
+          <View style={{ width: 150, alignItems: 'center' }}>
+            <CustomButton
+              color="black"
+              onPress={() => handleSelect('columnar')}
+            >
+              Columnar
+            </CustomButton>
+          </View>
 
-        <Button title='Cancel' onPress={onClose} />
+          <View />
+          <View style={{ width: 150, alignItems: 'center' }}>
+            <CustomButton color="black" onPress={() => handleSelect('grid')}>
+              Grid
+            </CustomButton>
+          </View>
+        </View>
+        <View
+          style={{
+            alignSelf: 'flex-start',
+            marginLeft: '15%',
+            marginVertical: 10,
+          }}
+        >
+          <CustomButton color="red" onPress={onClose}>
+            Cancel
+          </CustomButton>
+        </View>
       </View>
     </Modal>
   );
@@ -36,5 +64,10 @@ const styles = StyleSheet.create({
   },
   btns: {
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'silver',
+    borderRadius: 20,
+    height: '50%',
   },
 });
