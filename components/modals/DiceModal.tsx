2f9c43bd21e9657850edfc4221799b373a35d25d
Samuel Holmes <samuelholmes30@gmail.com>
2021-05-06 15:08:55 -0500

    designing dice modal:

--- components/modals/DiceModal.tsx
+++ components/modals/DiceModal.tsx
@@ -1,4 +1,11 @@
-import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import React from 'react';
 
@@ -9,10 +16,41 @@ interface DiceModalProps {
 export const DiceModal = (props: DiceModalProps) => {
   const { visible, setDiceModalVisible } = props;
   return (
-    <Modal visible={visible} animationType="fade">
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Die Modal TBD</Text>
-        <Button title="Close" onPress={() => setDiceModalVisible(false)} />
+    <Modal visible={visible} animationType='fade'>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+        }}
+      >
+        <View
+          style={{
+            height: '50%',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Text style={{ color: 'white', textAlign: 'center' }}>
+            Die Modal TBD
+          </Text>
+
+          <TouchableOpacity onPress={() => setDiceModalVisible(false)}>
+            <View
+              style={{
+                width: 200,
+                borderColor: 'white',
+                borderWidth: 2,
+                borderRadius: 5,
+                alignItems: 'center',
+                height: 30,
+                justifyContent: 'center',
+              }}
+            >
+              <Text style={{ color: 'white' }}>Close</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </Modal>
   );
