17f6d81037dc91aebba8e58d0a45d9d072e50d44
Samuel Holmes <samuelholmes30@gmail.com>
2021-05-07 08:17:35 -0500

    test

--- components/PlayersGrid.tsx
+++ components/PlayersGrid.tsx
@@ -5,6 +5,7 @@ import { CustomButton } from './CustomButton';
 import { DiceModal } from './modals/DiceModal';
 import { Foundation } from '@expo/vector-icons';
 import { Player } from '../screens/ActivePlayersScreen';
+import { TwoPlayer } from '../components/layouts/TwoPlayer';
 interface PlayersGrid {
   players: Player[];
   setPlayers: React.Dispatch<React.SetStateAction<Player[]>>;
@@ -269,8 +270,8 @@ export const PlayersGrid = (props: PlayersGrid) => {
   return (
     <View style={styles.content}>
       {players.length === 4 && fourPlayerFormat()}
-      {/* {players.length === 2 && <TwoPlayer />} */}
-      {players.length < 4 && (
+      {players.length === 2 && <TwoPlayer />}
+      {players.length !== 4 && players.length !== 2 && (
         <View>
           <Text style={{ color: 'white' }}>Unsupported format</Text>
         </View>
