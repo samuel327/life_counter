00f02264df7b85438be32ce64a52ef7e1a666170
Samuel Holmes <samuelholmes30@gmail.com>
2021-05-08 09:29:19 -0500

    styling

--- components/modals/LayoutModal.tsx
+++ components/modals/LayoutModal.tsx
@@ -1,8 +1,8 @@
-import { Modal, StyleSheet, Text, View } from 'react-native';
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { CustomButton } from '../CustomButton';
 import React from 'react';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 interface LayoutModalProps {
   visible: boolean;
   onClose: any;
@@ -17,39 +17,71 @@ export const LayoutModal = (props: LayoutModalProps) => {
   }
   return (
     <Modal visible={visible}>
-      <View style={styles.content}>
-        <Text
-          style={{ fontSize: 22, alignSelf: 'flex-start', marginLeft: '12%' }}
-        >
-          Choose a layout:
-        </Text>
-        <View style={styles.btns}>
-          <View style={{ width: 150, alignItems: 'center' }}>
-            <CustomButton
-              color="black"
-              onPress={() => handleSelect('columnar')}
-            >
-              Columnar
-            </CustomButton>
-          </View>
-
-          <View />
-          <View style={{ width: 150, alignItems: 'center' }}>
-            <CustomButton color="black" onPress={() => handleSelect('grid')}>
-              Grid
-            </CustomButton>
-          </View>
-        </View>
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
         <View
           style={{
-            alignSelf: 'flex-start',
-            marginLeft: '15%',
-            marginVertical: 10,
+            flex: 1,
+            alignItems: 'flex-end',
+
+            justifyContent: 'center',
+            marginVertical: 50,
           }}
         >
-          <CustomButton color="red" onPress={onClose}>
-            Cancel
-          </CustomButton>
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <TouchableOpacity
+              onPress={onClose}
+              style={{
+                backgroundColor: 'grey',
+                borderRadius: 37.5,
+                height: 75,
+                width: 75,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: 'black',
+                  height: 70,
+                  width: 70,
+                  borderRadius: 35,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <MaterialCommunityIcons name='close' size={50} color='red' />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.content}>
+          <Text
+            style={{
+              fontSize: 22,
+              alignSelf: 'flex-start',
+              marginLeft: '12%',
+              color: 'white',
+            }}
+          >
+            Choose a layout:
+          </Text>
+          <View style={styles.btns}>
+            <View style={{ width: 150, alignItems: 'center' }}>
+              <CustomButton
+                color='black'
+                onPress={() => handleSelect('columnar')}
+              >
+                Columnar
+              </CustomButton>
+            </View>
+
+            <View />
+            <View style={{ width: 150, alignItems: 'center' }}>
+              <CustomButton color='black' onPress={() => handleSelect('grid')}>
+                Grid
+              </CustomButton>
+            </View>
+          </View>
         </View>
       </View>
     </Modal>
@@ -58,9 +90,10 @@ export const LayoutModal = (props: LayoutModalProps) => {
 
 const styles = StyleSheet.create({
   content: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 10,
+    //justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black',
   },
   btns: {
     flexDirection: 'row',
@@ -69,5 +102,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'silver',
     borderRadius: 20,
     height: '50%',
+  },
+  text: {
+    color: 'white',
+    fontSize: 22,
   },
 });
