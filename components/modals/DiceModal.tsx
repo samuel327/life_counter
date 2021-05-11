8c4501d4b1baa151f7c1274d2be4270df986810c
Samuel Holmes <samuelholmes30@gmail.com>
2021-05-11 08:18:25 -0500

    dice modal styling

--- components/modals/DiceModal.tsx
+++ components/modals/DiceModal.tsx
@@ -20,101 +20,76 @@ export const DiceModal = (props: DiceModalProps) => {
   const [rolledNum, setRolledNum] = useState<number>();
   return (
     <Modal visible={visible} animationType='fade'>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'grey',
-        }}
-      >
-        <View
-          style={{
-            height: '50%',
-            justifyContent: 'space-between',
-          }}
-        >
-          {!rolledNum && (
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <TouchableOpacity
-                onPress={() => {
-                  setRolledNum(Math.floor(Math.random() * 20 + 1));
-                  setTimeout(() => {
-                    setRolledNum(undefined);
-                  }, 1000);
-                }}
-              >
-                <View
-                  style={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Image
-                    source={require('../../assets/dice_images/skuff_d20.png')}
-                  />
-                </View>
-              </TouchableOpacity>
-            </View>
-          )}
-          {rolledNum && (
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 90,
-                height: 210,
-              }}
-            >
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 50,
-                  textAlign: 'center',
-                }}
-              >
-                {rolledNum}
-              </Text>
-            </View>
-          )}
-          <TouchableOpacity onPress={() => setDiceModalVisible(false)}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 100,
+      <View style={styles.content}>
+        {!rolledNum && (
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                setRolledNum(Math.floor(Math.random() * 20 + 1));
+                setTimeout(() => {
+                  setRolledNum(undefined);
+                }, 1000);
               }}
             >
               <View
                 style={{
-                  width: 200,
-                  //borderColor: 'grey',
-                  borderWidth: 6,
-                  borderRadius: 5,
-                  alignItems: 'center',
-                  height: 50,
                   justifyContent: 'center',
-                  borderColor: 'rgba(1, 150, 150, .9)',
+                  alignItems: 'center',
                 }}
               >
-                <Text
-                  style={{ color: 'white', fontSize: 22, textAlign: 'center' }}
-                >
-                  Close
-                </Text>
+                <Image
+                  source={require('../../assets/dice_images/skuff_d20.png')}
+                />
               </View>
-            </View>
-          </TouchableOpacity>
-        </View>
+            </TouchableOpacity>
+          </View>
+        )}
+        {rolledNum && (
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+
+              height: 300,
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 50,
+                textAlign: 'center',
+              }}
+            >
+              {rolledNum}
+            </Text>
+          </View>
+        )}
+        <TouchableOpacity onPress={() => setDiceModalVisible(false)}>
+          <View style={styles.closeBtnContainer}>
+            <Text style={styles.closeBtnTxt}>Close</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </Modal>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+  },
+  closeBtnContainer: {
+    width: 200,
+    borderWidth: 6,
+    borderRadius: 5,
+    alignItems: 'center',
+    height: 50,
+    justifyContent: 'center',
+    borderColor: 'rgba(1, 150, 150, .9)',
+    marginBottom: 50,
+  },
+  closeBtnTxt: { color: 'white', fontSize: 22, textAlign: 'center' },
+});
